cd86f7af425896157899fb0d057aeca37108264d
Dave Gordon <helloitsdave@hotmail.com>
2024-01-01 22:48:59 +0000

    [backend] feat: Adding backend implementation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # notes-app
 
-Building a note-app FE + BE + DB with the freecodecamp tutorial[full-stack-project-tutorial-create-a-notes-app-using-react-and-node-js](https://www.freecodecamp.org/news/full-stack-project-tutorial-create-a-notes-app-using-react-and-node-js/)
+Building a note-app FE + BE + DB with the freecodecamp tutorial
+
+[full-stack-project-tutorial-create-a-notes-app-using-react-and-node-js](https://www.freecodecamp.org/news/full-stack-project-tutorial-create-a-notes-app-using-react-and-node-js/)
---
+++ backend/src/index.ts
@@ -0,0 +1,76 @@
+import { PrismaClient } from "@prisma/client";
+import express from 'express'
+import cors from 'cors'
+
+const prisma = new PrismaClient();
+const app = express()
+const PORT = 5001
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await prisma.note.findMany();
+    res.json(notes);
+  });
+
+app.post("/api/notes", async (req, res) => {
+const { title, content } = req.body;
+
+if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+}
+
+try {
+    const note = await prisma.note.create({
+    data: { title, content },
+    });
+    res.json(note);
+} catch (error) {
+    res.status(500).send("Oops, something went wrong");
+}
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+    const { title, content } = req.body;
+    const id = parseInt(req.params.id);
+  
+    if (!title || !content) {
+      return res.status(400).send("title and content fields required");
+    }
+  
+    if (!id || isNaN(id)) {
+      return res.status(400).send("ID must be a valid number");
+    }
+  
+    try {
+      const updatedNote = await prisma.note.update({
+        where: { id },
+        data: { title, content },
+      });
+      res.json(updatedNote);
+    } catch (error) {
+      res.status(500).send("Oops, something went wrong");
+    }
+  });
+
+  app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    if (!id || isNaN(id)) {
+      return res.status(400).send("ID field required");
+    }
+  
+    try {
+      await prisma.note.delete({
+        where: { id },
+      });
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).send("Oops, something went wrong");
+    }
+  });
+
+app.listen(PORT, () => {
+    console.log("server running on localhost", PORT);
+  });
