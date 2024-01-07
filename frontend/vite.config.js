9994ffe0209fb18614f356df4252041fe3c11356
Dave Gordon <helloitsdave@gmail.com>
2024-01-07 17:16:38 +0000

    [readme] chore: Updating config for coverage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,9 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+    },
   },
 });
