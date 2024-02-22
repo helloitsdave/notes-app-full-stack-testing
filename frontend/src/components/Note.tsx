8bcaf7fb77bf6dcabc36f6fc0a01b3cf0c3c2d63
Dave <43282177+helloitsdave@users.noreply.github.com>
2024-02-22 00:02:00 +0000

    [frontend] feat: Auto deploy after main merge (#28)

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,30 +1,40 @@
 import type NoteType from "../types/note";
 
 interface NoteProps {
-    note: NoteType;
-    handleEdit: () => void;
-    deleteNote: () => void;
+  note: NoteType;
+  handleEdit: () => void;
+  deleteNote: () => void;
 }
 
 const Note: React.FC<NoteProps> = ({ note, handleEdit, deleteNote }) => {
+  const handleDeleteMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // Prevent the click event from propagating to the parent container
+    e.stopPropagation();
 
-    const handleDeleteMouseDown = (e: React.MouseEvent<HTMLButtonElement>) => {
-        // Prevent the click event from propagating to the parent container
-        e.stopPropagation();
-
-        // Call the deleteNote function
-        deleteNote();
-    };
-    return (
-        <div data-testid="note" className="note-item" onClick={handleEdit}>
-            <h3 data-testid="note-title">{note.title}</h3>
-            <p className="note-content" data-testid="note-content">{note.content}</p>
-            <p className="note-updated" data-testid="note-updated">Updated {new Date(note.updatedAt ?? "").toLocaleDateString()}</p>
-            <div className="notes-header">
-                    <button data-testid='note-delete-button' onClick={handleDeleteMouseDown}>X</button>
-            </div>
-        </div>
-    );
+    // Call the deleteNote function
+    deleteNote();
+  };
+  return (
+    <div data-testid="note" className="note-item" onClick={handleEdit}>
+      <h3 className="note-title" data-testid="note-title">
+        {note.title}
+      </h3>
+      <p className="note-content" data-testid="note-content">
+        {note.content}
+      </p>
+      <div className="notes-header">
+        <p className="note-updated" data-testid="note-updated">
+          Updated {new Date(note.updatedAt ?? "").toLocaleDateString()}
+        </p>
+        <button
+          data-testid="note-delete-button"
+          onClick={handleDeleteMouseDown}
+        >
+          X
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Note;
