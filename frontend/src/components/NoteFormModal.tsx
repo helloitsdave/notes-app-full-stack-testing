e201141db05606ce9fc16ee93d9a44a233d6150e
Dave Gordon <helloitsdave@gmail.com>
2024-03-03 13:23:05 +0000

    [frontend] feat: Adding NoteFormModal component

---
+++ frontend/src/components/NoteFormModal.tsx
@@ -0,0 +1,32 @@
+
+import { Button, Modal } from "antd";
+import NoteForm from "./NoteForm";
+import type NoteType from "../types/note";
+
+interface ModalNoteFormProps {
+  addNote: (newNote: NoteType) => void;
+  updateNote: (updatedNote: NoteType) => void;
+  selectedNote: NoteType | null;
+  handleCancel: () => void;
+  isModalVisible: boolean;
+}
+
+ const ModalNoteForm: React.FC<ModalNoteFormProps>  = (props) => {
+  return (
+    <Modal
+      title="Note Form"
+      open={props.isModalVisible}
+      onCancel={props.handleCancel}
+      footer={null}
+    >
+      <NoteForm
+        onCancel={props.handleCancel}
+        selectedNote={props.selectedNote}
+        addNote={props.addNote}
+        updateNote={props.updateNote}
+      />
+    </Modal>
+  );
+}
+
+export default ModalNoteForm;
