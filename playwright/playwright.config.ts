0cf73481742ab84d50ccfa530928c07b431b4866
Dave <43282177+helloitsdave@users.noreply.github.com>
2024-03-16 21:50:40 +0000

    [playwright] feat: Add linting (#32)

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 2 * 60 * 1000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure', 
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -36,7 +37,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-     }
+    },
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
--- playwright/tests/note.spec.ts
+++ playwright/tests/note.spec.ts
@@ -9,60 +9,82 @@ const EDITED_NOTE_CONTENT = `Edited note content ${TIMESTAMP}`;
 let page: Page;
 let notesApi: Promise<Response>;
 
-const timeout = 80 * 1000;
+const timeout = 60 * 1000;
 
 test.beforeAll(async ({ browser }, { timeout }) => {
   page = await browser.newPage();
 
   /** Free tier on render.com may take 60 seconds to startup */
-  const notesApi = page.waitForResponse((response) => response.url().includes("/api/notes") && response.status() === 200, { timeout });
+  notesApi = page.waitForResponse(
+    (response) =>
+      response.url().includes('/api/notes') && response.status() === 200,
+    { timeout }
+  );
 
   await page.goto('/', { timeout });
 });
 
-test('Notes App e2e', async () => {
-
+test('Notes App e2e flow', async () => {
   await test.step('Should have loaded', async () => {
-    await notesApi;
+    /** Wait for api response to complete */
+    const notesApiResponse = await notesApi;
+    const json = await notesApiResponse.json();
 
-    expect(page).toHaveTitle(/Notes/);
-    expect(page.getByTestId('spinner-container')).not.toBeVisible({ timeout });
+    /** Expect page to have loaded */
+    await expect(page).toHaveTitle(/Notes/, { timeout });
+    await expect(page.getByTestId('spinner-container')).toBeHidden({ timeout });
+
+    /** Expect at least one note to exist */
+    expect(json.length).toBeGreaterThan(0);
   });
 
   await test.step('Should be able to Add a Note', async () => {
     await page.getByRole('button', { name: 'Add a Note' }).click();
     await page.getByPlaceholder('Title').fill(NOTE_TITLE);
-    await page.getByPlaceholder('Content').fill(NOTE_CONTENT);    
+    await page.getByPlaceholder('Content').fill(NOTE_CONTENT);
     await page.getByRole('button', { name: 'Add Note' }).click();
   });
 
   await test.step('Should be able to see the new Note', async () => {
     await expect(page.getByTestId('note-title').first()).toHaveText(NOTE_TITLE);
-    await expect(page.getByTestId('note-content').first()).toHaveText(NOTE_CONTENT);
+    await expect(page.getByTestId('note-content').first()).toHaveText(
+      NOTE_CONTENT
+    );
   });
 
   await test.step('Should be able to Delete a Note', async () => {
     await page.getByTestId('note-delete-button').first().click();
-    await expect(page.getByTestId('note-title').first()).not.toHaveText(NOTE_TITLE);
+    await expect(page.getByTestId('note-title').first()).not.toHaveText(
+      NOTE_TITLE
+    );
   });
 
   await test.step('Should be able to Edit a Note', async () => {
     await page.getByTestId('note').first().click();
-    expect(page.getByText('Save')).toBeVisible();
-    expect(page.getByText('Cancel')).toBeVisible();
+    await expect(page.getByText('Save')).toBeVisible();
+    await expect(page.getByText('Cancel')).toBeVisible();
 
     await page.getByPlaceholder('Title').fill(EDITED_NOTE_TITLE);
     await page.getByPlaceholder('Content').fill(EDITED_NOTE_CONTENT);
     await page.getByRole('button', { name: 'Save' }).click();
-    await expect(page.getByTestId('note-title').first()).toHaveText(EDITED_NOTE_TITLE);
-    await expect(page.getByTestId('note-content').first()).toHaveText(EDITED_NOTE_CONTENT);
+    await expect(page.getByTestId('note-title').first()).toHaveText(
+      EDITED_NOTE_TITLE
+    );
+    await expect(page.getByTestId('note-content').first()).toHaveText(
+      EDITED_NOTE_CONTENT
+    );
   });
 
   await test.step('Should not be able to add Note without title', async () => {
     await page.getByRole('button', { name: 'Add a Note' }).click();
     await page.getByPlaceholder('Content').fill(NOTE_CONTENT);
     await page.getByRole('button', { name: 'Add Note' }).click();
-    await expect(page.getByTestId('note-title').first()).not.toHaveText(NOTE_TITLE, { timeout });
-    await expect(page.getByTestId('note-content').first()).not.toHaveText(NOTE_CONTENT);
+    await expect(page.getByTestId('note-title').first()).not.toHaveText(
+      NOTE_TITLE,
+      { timeout }
+    );
+    await expect(page.getByTestId('note-content').first()).not.toHaveText(
+      NOTE_CONTENT
+    );
   });
 });
