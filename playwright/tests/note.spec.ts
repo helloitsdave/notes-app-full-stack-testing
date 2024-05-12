c1f235fe93ddce67755d23a09d20a3847b87e3d4
Dave <43282177+helloitsdave@users.noreply.github.com>
2024-05-12 14:04:45 +0100

    Allure (#42)
    * [playwrite] feat: Allure reporting for production test

--- playwright/tests/note.spec.ts
+++ playwright/tests/note.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, Page } from '@playwright/test';
 import { faker } from '@faker-js/faker';
+import { allure } from 'allure-playwright';
 import LoginPage from '../pages/LoginPage';
 import NotesPage from '../pages/NotesPage';
 
@@ -14,37 +15,35 @@ const EDITED_NOTE_TITLE = faker.lorem.sentence();
 const EDITED_NOTE_CONTENT = faker.lorem.lines({ min: 1, max: 5 });
 
 test.beforeAll(async ({ browser }) => {
+  await allure.feature('Notes e2e flow');
   page = await browser.newPage();
   loginPage = new LoginPage(page);
   notesPage = new NotesPage(page);
   await loginPage.goto();
 });
 
-test('Notes App e2e flow', { tag: ['@PRODUCTION'] }, async () => {
-  await test.step('Should see notes page login', async () => {
+test.describe('Notes App e2e flow', { tag: ['@PRODUCTION'] }, async () => {
+  test('Should see notes page login', async () => {
     await loginPage.login('dave', 'test');
     await expect(page).toHaveTitle(/Notes/, { timeout });
     await expect(notesPage.spinnerContainer()).toBeHidden({ timeout });
   });
 
-  await test.step('Should be able to Add a Note', async () => {
+  test('Should be able to Add a Note', async () => {
     await notesPage.addNote({
       title: NOTE_TITLE,
       content: NOTE_CONTENT,
     });
-  });
-
-  await test.step('Should be able to see the new Note', async () => {
     await expect(notesPage.noteTitle().first()).toHaveText(NOTE_TITLE);
     await expect(notesPage.noteContent().first()).toHaveText(NOTE_CONTENT);
   });
 
-  await test.step('Should be able to Delete a Note', async () => {
+  test('Should be able to Delete a Note', async () => {
     await notesPage.deleteNote();
     await expect(notesPage.noteTitle().first()).not.toHaveText(NOTE_TITLE);
   });
 
-  await test.step('Should be able to Edit a Note', async () => {
+  test('Should be able to Edit a Note', async () => {
     await notesPage.editNote({
       title: EDITED_NOTE_TITLE,
       content: EDITED_NOTE_CONTENT,
@@ -55,7 +54,7 @@ test('Notes App e2e flow', { tag: ['@PRODUCTION'] }, async () => {
     );
   });
 
-  await test.step('Should not be able to add Note without title', async () => {
+  test('Should not be able to add Note without title', async () => {
     await notesPage.addNote({
       title: '',
       content: NOTE_TITLE,
@@ -69,7 +68,7 @@ test('Notes App e2e flow', { tag: ['@PRODUCTION'] }, async () => {
     await expect(notesPage.noteTitle().first()).not.toHaveText(NOTE_CONTENT);
   });
 
-  await test.step('Should be able to logout', async () => {
+  test('Should be able to logout', async () => {
     await page.getByRole('button', { name: 'Logout' }).click();
     await expect(page.getByPlaceholder('Username')).toBeVisible();
   });
