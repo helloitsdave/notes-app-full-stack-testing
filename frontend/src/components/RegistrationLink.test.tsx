7786c14dd8e5cf1c3930f0cd9346f5613a5d9726
Dave <43282177+helloitsdave@users.noreply.github.com>
2024-06-15 14:17:19 +0100

    [frontend] feat: Adding github info (#54)

--- frontend/src/components/RegistrationLink.test.tsx
+++ frontend/src/components/RegistrationLink.test.tsx
@@ -4,11 +4,21 @@ import RegistrationLink from './RegistrationLink';
 describe('RegistrationLink', () => {
   it('should render the registration link', () => {
     render(<RegistrationLink onRegister={false} />);
-    expect(screen.getByText('Welcome to the e-notes app')).toBeInTheDocument();
     expect(
-      screen.getByText('Sign up for your free account'),
+      screen.getByText(
+        'This is a simple app to demonstrate different automated testing techniques.',
+      ),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText('Sign up for your free e-notes account.'),
     ).toBeInTheDocument();
     expect(screen.getByText('Sign up')).toBeInTheDocument();
+
+    expect(
+      screen.getByText('View the source code on GitHub.'),
+    ).toBeInTheDocument();
+
+    expect(screen.getByAltText('Github link')).toBeInTheDocument();
   });
 
   it('should render the registration link with account created successfully message', () => {
--- frontend/src/components/RegistrationLink.tsx
+++ frontend/src/components/RegistrationLink.tsx
@@ -1,13 +1,30 @@
+/* eslint-disable prettier/prettier */
 const RegistrationLink = (props: { onRegister: boolean }) => {
   return (
-    <div>
-      <div className="registration-link">
-        <h1>e-notes</h1>
-      </div>
+    <div className="registration-container">
       {!props.onRegister ? (
         <div className="registration-link-container">
-          <p>Welcome to the e-notes app</p>
-          <p>Sign up for your free account</p>
+          <div className="github-info-container">
+            <p>
+              This is a simple app to demonstrate different automated testing
+              techniques. 
+            </p>
+            <p>
+            View the source code on GitHub.
+            </p>
+            <a
+              href="https://github.com/helloitsdave/notes-app-full-stack-testing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="github-image"
+                src="github-mark.svg"
+                alt="Github link"
+              />
+            </a>
+          </div>
+          <p>Sign up for your free e-notes account.</p>
           <a className="nav-link" href="/register">
             Sign up
           </a>
