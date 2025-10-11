684f4d0785da9161fab2a71212ee57da6885321a
Dave Gordon <helloitsdave@gmail.com>
2025-10-11 15:19:25 +0100

    Revert "[playwright] chore: Correct prod api url"
    This reverts commit a2201e58a7767d661b4d83136fa448af823c3244.

--- playwright/helpers/getApiUrl.ts
+++ playwright/helpers/getApiUrl.ts
@@ -5,7 +5,7 @@ const getApiUrl = async () => {
 
   const apiURL = baseURL?.includes('localhost')
     ? 'http://localhost:5000'
-    : 'https://www.noteapp.co.uk';
+    : 'https://notes-app-full-stack-be.onrender.com';
 
   if (!baseURL) {
     throw new Error('Base URL is required to make API requests');
